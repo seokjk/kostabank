6cb95c81e2acf977911ed80d84af590dc188f5b3
wonsungyoun <sungyounet@naver.com>
2016-06-15 11:18:36 +0900

    적금 금리에 관한 비즈니스 로직

---
+++ kostabank_ver2/src/main/java/org/kosta/kostabank/model/vo/SavingsVO.java
@@ -0,0 +1,68 @@
+package org.kosta.kostabank.model.vo;
+
+public class SavingsVO {  
+	private AccountVO accountVO;
+	private String automaticNo;
+	private AccountRatesVO accountRatesVO;
+	private int monthlyPayment;
+	public int transferBoolean;
+	public String paybackNo;
+	public SavingsVO(AccountVO accountVO, String automaticNo,
+			AccountRatesVO accountRatesVO, int monthlyPayment,
+			int transferBoolean, String paybackNo) {
+		super();
+		this.accountVO = accountVO;
+		this.automaticNo = automaticNo;
+		this.accountRatesVO = accountRatesVO;
+		this.monthlyPayment = monthlyPayment;
+		this.transferBoolean = transferBoolean;
+		this.paybackNo = paybackNo;
+	}
+	public SavingsVO() {
+		super();
+	}
+	public AccountVO getAccountVO() {
+		return accountVO;
+	}
+	public void setAccountVO(AccountVO accountVO) {
+		this.accountVO = accountVO;
+	}
+	public String getAutomaticNo() {
+		return automaticNo;
+	}
+	public void setAutomaticNo(String automaticNo) {
+		this.automaticNo = automaticNo;
+	}
+	public AccountRatesVO getAccountRatesVO() {
+		return accountRatesVO;
+	}
+	public void setAccountRatesVO(AccountRatesVO accountRatesVO) {
+		this.accountRatesVO = accountRatesVO;
+	}
+	public int getMonthlyPayment() {
+		return monthlyPayment;
+	}
+	public void setMonthlyPayment(int monthlyPayment) {
+		this.monthlyPayment = monthlyPayment;
+	}
+	public int getTransferBoolean() {
+		return transferBoolean;
+	}
+	public void setTransferBoolean(int transferBoolean) {
+		this.transferBoolean = transferBoolean;
+	}
+	public String getPaybackNo() {
+		return paybackNo;
+	}
+	public void setPaybackNo(String paybackNo) {
+		this.paybackNo = paybackNo;
+	}
+	@Override
+	public String toString() {
+		return "SavingsVO [accountVO=" + accountVO + ", automaticNo="
+				+ automaticNo + ", accountRatesVO=" + accountRatesVO
+				+ ", monthlyPayment=" + monthlyPayment + ", transferBoolean="
+				+ transferBoolean + ", paybackNo=" + paybackNo + "]";
+	}
+	
+}
