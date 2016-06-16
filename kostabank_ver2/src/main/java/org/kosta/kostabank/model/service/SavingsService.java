4386dae63b98fc8cc4cdc45dd154440132435eea
wonsungyoun <sungyounet@naver.com>
2016-06-16 10:09:14 +0900

    서비스 계층

--- kostabank_ver2/src/main/java/org/kosta/kostabank/model/service/SavingsService.java
+++ kostabank_ver2/src/main/java/org/kosta/kostabank/model/service/SavingsService.java
@@ -2,10 +2,11 @@ package org.kosta.kostabank.model.service;
 
 import java.util.List;
 
+import org.kosta.kostabank.model.vo.AccountRatesVO;
 import org.kosta.kostabank.model.vo.AccountTypeVO;
 
 public interface SavingsService {
 
 	public abstract List<AccountTypeVO> savingsProductlist(String accountType);
-
+	public AccountRatesVO accountNameFindAccountList(String accountName);
 }
--- kostabank_ver2/src/main/java/org/kosta/kostabank/model/service/SavingsServiceImpl.java
+++ kostabank_ver2/src/main/java/org/kosta/kostabank/model/service/SavingsServiceImpl.java
@@ -5,6 +5,7 @@ import java.util.List;
 import javax.annotation.Resource;
 
 import org.kosta.kostabank.model.dao.SavingsDAO;
+import org.kosta.kostabank.model.vo.AccountRatesVO;
 import org.kosta.kostabank.model.vo.AccountTypeVO;
 import org.springframework.stereotype.Service;
 
@@ -16,5 +17,8 @@ public class SavingsServiceImpl implements SavingsService {
 	public List<AccountTypeVO> savingsProductlist(String accountType) {
 		System.out.println(accountType);
 		return savingsDAO.savingsProductlist(accountType);
+	}
+	public AccountRatesVO accountNameFindAccountList(String accountName){
+		return savingsDAO.accountNameFindAccountList(accountName);
 	}
 }
