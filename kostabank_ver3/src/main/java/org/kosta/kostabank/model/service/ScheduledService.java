3843ef15437d03752f78cde330fdf090d547756c
kosta <kosta@kosta-HP>
2016-06-20 19:21:44 +0900

    ScheduledService 수정

--- kostabank_ver3/src/main/java/org/kosta/kostabank/model/service/ScheduledService.java
+++ kostabank_ver3/src/main/java/org/kosta/kostabank/model/service/ScheduledService.java
@@ -14,100 +14,107 @@ public class ScheduledService {
 	@Resource
 	private LoanDAO loanDAO;
 	
+	@Scheduled(cron="0 15 10 15 * ?")
+	public void withdraw(){
+		loanDAO.nowBalance();
+		loanDAO.withdraw();
+		System.out.println("10초마다 출금");
+	}
+	
 	//대출 : 매달 15일 오전 10시 15분 balanceSum에 이자 업데이트
 	@Scheduled(cron="0 15 10 15 * ?")
 	public void balanceSumUpdate() {
-		System.out.println("balanceSumUpdate");
 		loanDAO.balanceSumUpdate();
+		System.out.println("balanceSumUpdate");
 	}
 	//입출금 : 매일 12시에 일수와 잔액합계가 업데이트
 	@Scheduled(cron="0 0 12 * * ?")
 	public void scheduled() {
-		System.out.println("scheduled");
 		accountDAO.scheduled();
+		System.out.println("scheduled");
 	}
 	//입출금 : 각 금리달에 알맞은 달이 되면 업데이트
 	@Scheduled(cron="0 0 0 28 1 ?")
 	public void ratesMonth1() {
-		System.out.println("ratesMonth");
 		accountDAO.ratesMonth(1);
+		System.out.println("ratesMonth");
 	}
 	@Scheduled(cron="0 0 0 28 2 ?")
 	public void ratesMonth2() {
-		System.out.println("ratesMonth");
 		accountDAO.ratesMonth(1);
 		accountDAO.ratesMonth(2);
+		System.out.println("ratesMonth");
 	}
 	@Scheduled(cron="0 0 0 28 3 ?")
 	public void ratesMonth3() {
-		System.out.println("ratesMonth");
 		accountDAO.ratesMonth(1);
 		accountDAO.ratesMonth(3);
+		System.out.println("ratesMonth");
 	}
 	@Scheduled(cron="0 0 0 28 4 ?")
 	public void ratesMonth4() {
-		System.out.println("ratesMonth");
 		accountDAO.ratesMonth(1);
 		accountDAO.ratesMonth(2);
 		accountDAO.ratesMonth(4);
+		System.out.println("ratesMonth");
 	}
 	@Scheduled(cron="0 0 0 28 5 ?")
 	public void ratesMonth5() {
-		System.out.println("ratesMonth");
 		accountDAO.ratesMonth(1);
 		accountDAO.ratesMonth(5);
+		System.out.println("ratesMonth");
 	}
 	@Scheduled(cron="0 0 0 28 6 ?")
 	public void ratesMonth6() {
-		System.out.println("ratesMonth");
 		accountDAO.ratesMonth(1);
 		accountDAO.ratesMonth(2);
 		accountDAO.ratesMonth(3);
 		accountDAO.ratesMonth(6);
+		System.out.println("ratesMonth");
 	}
 	@Scheduled(cron="0 0 0 28 7 ?")
 	public void ratesMonth7() {
-		System.out.println("ratesMonth");
 		accountDAO.ratesMonth(1);
 		accountDAO.ratesMonth(7);
+		System.out.println("ratesMonth");
 	}
 	@Scheduled(cron="0 0 0 28 8 ?")
 	public void ratesMonth8() {
-		System.out.println("ratesMonth");
 		accountDAO.ratesMonth(1);
 		accountDAO.ratesMonth(2);
 		accountDAO.ratesMonth(4);
 		accountDAO.ratesMonth(8);
+		System.out.println("ratesMonth");
 	}
 	@Scheduled(cron="0 0 0 28 9 ?")
 	public void ratesMonth9() {
-		System.out.println("ratesMonth");
 		accountDAO.ratesMonth(1);
 		accountDAO.ratesMonth(3);
 		accountDAO.ratesMonth(9);
+		System.out.println("ratesMonth");
 	}
 	@Scheduled(cron="0 0 0 28 10 ?")
 	public void ratesMonth10() {
-		System.out.println("ratesMonth");
 		accountDAO.ratesMonth(1);
 		accountDAO.ratesMonth(2);
 		accountDAO.ratesMonth(5);
 		accountDAO.ratesMonth(10);
+		System.out.println("ratesMonth");
 	}
 	@Scheduled(cron="0 0 0 28 11 ?")
 	public void ratesMonth11() {
-		System.out.println("ratesMonth");
 		accountDAO.ratesMonth(1);
 		accountDAO.ratesMonth(11);
+		System.out.println("ratesMonth");
 	}
 	@Scheduled(cron="0 0 0 28 12 ?")
 	public void ratesMonth12() {
-		System.out.println("ratesMonth");
 		accountDAO.ratesMonth(1);
 		accountDAO.ratesMonth(2);
 		accountDAO.ratesMonth(3);
 		accountDAO.ratesMonth(4);
 		accountDAO.ratesMonth(6);
 		accountDAO.ratesMonth(12);
+		System.out.println("ratesMonth");
 	}
 }
