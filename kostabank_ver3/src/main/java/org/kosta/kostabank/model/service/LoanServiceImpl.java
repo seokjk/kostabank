f9e6614976de3c5adae257f05691a00024a4f7ef
kosta <kosta@kosta-HP>
2016-06-20 19:21:13 +0900

    LoanServiceImpl 수정

--- kostabank_ver3/src/main/java/org/kosta/kostabank/model/service/LoanServiceImpl.java
+++ kostabank_ver3/src/main/java/org/kosta/kostabank/model/service/LoanServiceImpl.java
@@ -1,30 +1,22 @@
 package org.kosta.kostabank.model.service;
 
-import java.util.List;
-
 import javax.annotation.Resource;
 
 import org.kosta.kostabank.model.dao.LoanDAO;
+import org.kosta.kostabank.model.vo.AccountTypeVO;
+import org.kosta.kostabank.model.vo.AccountVO;
+import org.kosta.kostabank.model.vo.CustomerVO;
 import org.kosta.kostabank.model.vo.LoanAccountVO;
+import org.kosta.kostabank.model.vo.LoanVO;
 import org.springframework.stereotype.Service;
 @Service
 public class LoanServiceImpl implements LoanService{
 	@Resource
 	private LoanDAO loanDAO;
-	
-	//대출 이름 리스트
-	@Override
-	public List<String> loanNameList() {
-		return loanDAO.loanNameList();
-	}
-	//대출 전체 리스트
-	@Override
-	public LoanAccountVO loanList(String accountName) {
-		return loanDAO.loanList(accountName);
-	}
+
 	@Override
 	public int checkLoan(int maxMoney){
-		return loanDAO.checkLoan(maxMoney);
+		return loanDAO.checkLoan(maxMoney);	
 	}
 	@Override
 	public LoanAccountVO loanData(String name) {
@@ -33,5 +25,22 @@ public class LoanServiceImpl implements LoanService{
 	@Override
 	public LoanAccountVO checkPeriod(String period){
 		return loanDAO.checkPeriod(period);
+	}
+	//대출눌렀을때, 대출계좌, 대출 테이블에 insert 
+	@Override
+	public LoanVO loanSuccess(CustomerVO cvo, LoanVO vo, AccountTypeVO avo) {
+		int randomVal=(int)(Math.random()* 8999)+1000;
+		int randomValSe=(int)(Math.random() * 899)+100;
+		String accountNo = "219-"+randomVal+"-"+randomValSe+"-28";
+		AccountVO accountVO = new AccountVO(accountNo, avo, vo, cvo);
+		loanDAO.createAccount(accountVO);	
+		vo.setLoanAccountNo(accountVO.getAccountNo());
+		vo.setAccountName(avo.getAccountName());
+		loanDAO.createLoan(vo);
+		return vo;
+	}
+	@Override
+	public LoanVO selectLoan(String accountNo) {
+		return loanDAO.selectLoan(accountNo);
 	}
 }
