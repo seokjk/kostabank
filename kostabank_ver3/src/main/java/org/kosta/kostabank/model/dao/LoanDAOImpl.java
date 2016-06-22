c85c0f49e832cfb6ae8a2914e65c9f25dc00dd31
kosta <kosta@kosta-HP>
2016-06-22 11:32:06 +0900

    LoanDAOImpl 수정

--- kostabank_ver3/src/main/java/org/kosta/kostabank/model/dao/LoanDAOImpl.java
+++ kostabank_ver3/src/main/java/org/kosta/kostabank/model/dao/LoanDAOImpl.java
@@ -1,5 +1,7 @@
 package org.kosta.kostabank.model.dao;
 
+import java.util.List;
+
 import javax.annotation.Resource;
 
 import org.kosta.kostabank.model.vo.AccountVO;
@@ -9,47 +11,95 @@ import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public class LoanDAOImpl implements LoanDAO{
+public class LoanDAOImpl implements LoanDAO {
 	@Resource
 	private SqlSessionTemplate template;
 
-	//대출 금액 체크
+	// 대출 계좌 이름 리스트
 	@Override
-	public int checkLoan(int maxMoney){
-		return template.selectOne("loan.checkLoan",maxMoney);
+	public List<String> loanNameList() {
+		return template.selectList("loan.loanNameList");
 	}
+
+	// 대출 전체 리스트
+	@Override
+	public LoanAccountVO loanList(String accountName) {
+		System.out.println("loanDAO : "
+				+ template.selectOne("loan.loanList", accountName));
+		return template.selectOne("loan.loanList", accountName);
+	}
+
+	// 대출 금액 체크
+	@Override
+	public int checkLoan(int maxMoney) {
+		return template.selectOne("loan.checkLoan", maxMoney);
+	}
+
 	@Override
 	public LoanAccountVO loanData(String name) {
 		return template.selectOne("loan.loanData", name);
 	}
+
 	@Override
-	public LoanAccountVO checkPeriod(String period){
+	public LoanAccountVO checkPeriod(String period) {
 		return template.selectOne("loan.checkPeriod", period);
 	}
+
 	@Override
 	public void createAccount(AccountVO accountVO) {
-		template.insert("loan.createAccount",accountVO);
+		template.insert("loan.createAccount", accountVO);
 	}
+
 	@Override
 	public void createLoan(LoanVO vo) {
-		template.insert("loan.createLoan",vo);
+		template.insert("loan.createLoan", vo);
 	}
+
 	@Override
 	public LoanVO selectLoan(String accountNo) {
-		return template.selectOne("loan.selectLoan",accountNo);
+		return template.selectOne("loan.selectLoan", accountNo);
 	}
-	
+
 	@Override
 	public void withdraw() {
 		template.update("loan.withdraw");
 	}
+
 	@Override
 	public void nowBalance() {
 		template.update("loan.nowBalance");
 	}
-	//balanceSum Update 구문
+
 	@Override
-	public int balanceSumUpdate() {
-		return template.update("loan.balanceSumUpdate");
+	public List<LoanVO> selectDealDetail() {
+		return template.selectList("loan.selectDealDetail");
 	}
+
+	@Override
+	public void transfer(List<LoanVO> list) {
+		for (int i = 0; i < list.size(); i++) {
+			template.insert("loan.transferWithdraw", list.get(i));
+			template.insert("loan.transferDeposit", list.get(i));
+		}
+	}
+
+	@Override
+	public List<LoanVO> dailyCheckBalance() {
+		return template.selectList("loan.dailyCheckBalance");
+	}
+
+	@Override
+	public void dailyCheckWithdraw(List<LoanVO> list) {
+		for (int i = 0; i < list.size(); i++) {
+			template.update("loan.dailyCheckWithdraw", list.get(i));
+		}
+	}
+
+	@Override
+	public void dailyCheckUpdate(List<LoanVO> list) {
+		for (int i = 0; i < list.size(); i++) {
+			template.update("loan.dailyCheckUpdate", list.get(i));
+		}
+	}
+
 }
